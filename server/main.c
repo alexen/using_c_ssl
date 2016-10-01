df2078ae6cf88098bfc0bcf9fc7486eba425707f
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-10-01 22:19:10 +0300

    server: Реализовано использование протокола SSL

--- server/main.c
+++ server/main.c
@@ -13,7 +13,7 @@
 #include <common/common.h>
 
 
-void do_server_loop( BIO* bio )
+int do_server_loop( SSL* ssl )
 {
      int n_read = 0;
      do
@@ -21,7 +21,7 @@ void do_server_loop( BIO* bio )
           char buf[ 80 ] = { 0 };
           for( int total_read = 0; total_read < sizeof( buf ); total_read += n_read )
           {
-               n_read = BIO_read( bio, buf + n_read, sizeof( buf ) - total_read );
+               n_read = SSL_read( ssl, buf + n_read, sizeof( buf ) - total_read );
                if( n_read <= 0 )
                {
                     break;
@@ -30,25 +30,77 @@ void do_server_loop( BIO* bio )
           }
      }
      while( n_read > 0 );
+
+     return (SSL_get_shutdown( ssl ) & SSL_RECEIVED_SHUTDOWN) ? 1 : 0;
 }
 
 
 void* server_thread( void* args )
 {
-     BIO* client = (BIO*) args;
+     SSL* ssl = (SSL*) args;
      pthread_detach( pthread_self() );
-     printf( "connection opened\n" );
-     do_server_loop( client );
-     printf( "connection closed\n" );
-     BIO_free( client );
+     if( SSL_accept( ssl ) <= 0 )
+     {
+          SSL_ERROR_INTERRUPT( "accepting SSL connection error" );
+     }
+     printf( "SSL connection opened\n" );
+     if( do_server_loop( ssl ) )
+     {
+          SSL_shutdown( ssl );
+     }
+     else
+     {
+          SSL_clear( ssl );
+     }
+     printf( "SSL connection closed\n" );
+     SSL_free( ssl );
      ERR_remove_state( 0 );
      return NULL;
 }
 
 
+int password_callback( char* buf, int size, int flags, void* userdata )
+{
+     const char* password = (const char*) userdata;
+     const int passwordLen = strlen( password );
+     strncpy( buf, password, passwordLen );
+     return passwordLen;
+}
+
+
+SSL_CTX* setup_server_ctx( const char* certfile, const char* pk_file, const char* pk_password )
+{
+     SSL_CTX* ctx = SSL_CTX_new( SSLv23_method() );
+     SSL_ERROR_INTERRUPT_IF( !ctx, "ssl context creating error" );
+     if( certfile && !SSL_CTX_use_certificate_chain_file( ctx, certfile ) )
+     {
+          SSL_ERROR_INTERRUPT( "using cert chain file error" );
+     }
+     if( pk_file )
+     {
+          if( pk_password )
+          {
+               SSL_CTX_set_default_passwd_cb( ctx, password_callback );
+               SSL_CTX_set_default_passwd_cb_userdata( ctx, (void*) pk_password );
+          }
+          if( !SSL_CTX_use_PrivateKey_file( ctx, pk_file, SSL_FILETYPE_PEM ) )
+          {
+               SSL_ERROR_INTERRUPT( "using private key file error" );
+          }
+     }
+     return ctx;
+}
+
+
 int main( int argc, char **argv )
 {
+     static const char* const certfile = "/home/alexen/worktrash/ssl/server.pem";
+     static const char* const pkfile = "/home/alexen/worktrash/ssl/server.pem";
+     static const char* const pkpassword = "111111";
+
      ssl_init();
+     ssl_seed_prng_bytes( 1024 );
+     SSL_CTX* ctx = setup_server_ctx( certfile, pkfile, pkpassword );
      BIO* acc = BIO_new_accept( "8080" );
      SSL_ERROR_INTERRUPT_IF( !acc, "create new accept error" );
      if( BIO_do_accept( acc ) <= 0 )
@@ -63,9 +115,13 @@ int main( int argc, char **argv )
                SSL_ERROR_INTERRUPT( "accept error" );
           }
           BIO* client = BIO_pop( acc );
-          const int errnum = pthread_create( &t_id, NULL, server_thread, client );
+          SSL* ssl = SSL_new( ctx );
+          SSL_ERROR_INTERRUPT_IF( !ssl, "creating SSL error" );
+          SSL_set_bio( ssl, client, client );
+          const int errnum = pthread_create( &t_id, NULL, server_thread, ssl );
           SYS_ERROR_INTERRUPT_IF( errnum != 0, errnum, "thread creating error" );
      }
+     SSL_CTX_free( ctx );
      BIO_free( acc );
      ssl_shutdown();
      return EXIT_SUCCESS;
